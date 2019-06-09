c02c29290ebe847d774240cfd40d8940a00410e0
DavidBermejoS <dbermejosimon@gmail.com>
2019-06-09 22:10:12 +0200

    HF-002: Borrado documentos exportados.

--- HTML_FILES/convert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo wkhtmltopdf --footer-center [page] --header-center 'Investigacion en tecnologías DataScience y MachineLearning con Python \n David Bermejo Simon \n Jose Luis Luengo Ramos' -T 25mm $1 $2
-
